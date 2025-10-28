38d73ceaa1eb5a7b3a696029d604785eea243898
ming <hongming_1212@hotmail.com>
2025-10-29 01:10:13 +0800

    feat: added necessary 3d models and glb files for 3D animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "#components/Loader.jsx";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full h-screen relative">
+      <Canvas
+        // Setting near, far clipping planes for the camera
+        camera={{ near: 0.1, far: 1000 }}
+        className="w-full h-screen bg-transparent"
+      >
+        {/* 3D content goes here */}
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
