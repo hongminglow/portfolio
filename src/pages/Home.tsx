9780b692d1ca474ce6fbf52cd993ebdb893b8944
ming <hongming_1212@hotmail.com>
2025-10-29 01:28:02 +0800

    added simple directional, ambient and sphere light for light testing on the 3d models

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "#components/Loader.jsx";
+import { Island } from "#models/Island.jsx";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition];
+  };
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -12,10 +26,15 @@ const Home = () => {
       >
         {/* 3D content goes here */}
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <spotLight />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Island position={islandPosition} />
         </Suspense>
       </Canvas>
     </section>
