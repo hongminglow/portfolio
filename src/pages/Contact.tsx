0cc62ee4bb6acc4f046208e4aac3750cc7cbbe45
ming <hongming_1212@hotmail.com>
2025-10-29 23:25:30 +0800

    feat: update env variable for mailing services in cntact screen

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Hong Ming Low",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "hongming_1212@hotmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
